e7b8d3da873651cb45bacfeb1dc7cd9c5ae679d4
gee-whiz <george.kapoya@fi.abb.com>
2021-03-13 09:16:55 +0200

    add run-tf2.sh

---
+++ run-tf2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --partition=gputest
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=0:15:00
+#SBATCH --mem=64G
+#SBATCH --account=project_2000859
+
+module purge
+module load tensorflow/2.0.0
+module list
+
+# export DATADIR=/scratch/project_2000859/demo/extracted
+
+set -xv
+srun python3 $*
