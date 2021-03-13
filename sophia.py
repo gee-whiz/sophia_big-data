62065b0fc02cff50e1a7b356f9ddf116b21109e1
gee-whiz <george.kapoya@fi.abb.com>
2021-03-13 11:47:57 +0200

    fix warnings

--- sophia.py
+++ sophia.py
@@ -5,11 +5,12 @@ import pip
 import glob
 from zipfile import ZipFile
 
+
 with ZipFile('dataset.zip', 'r') as zipObj:
    # Extract all the contents of zip file in current directory
    zipObj.extractall()
 
-path = r'dataset' # use your path
+path = r'dataset' 
 all_files = glob.glob(path + "/*.csv")
 
 
@@ -20,8 +21,9 @@ def get_fnames():
         file_names.append(df)
     return file_names
 
+#we join our csv files
 file_names = get_fnames()
-frame = pd.concat(file_names, axis=0, ignore_index=True)
+df = pd.concat(file_names, axis=0, ignore_index=True, sort=True)
 
-print(frame.size)
-print(frame.head())
+print(df.size)
+print(df.head().to_string())
