e8c5c3e3b0414ab9bc553040b02458a0e6a67f32
gee-whiz <george.kapoya@fi.abb.com>
2021-03-17 15:25:22 +0200

    update run script

--- run-tf2.sh
+++ run-tf2.sh
@@ -2,7 +2,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --partition=gputest
+#SBATCH --partition=test
+#SBATCH --gres=gpu:v100:1
 #SBATCH --time=0:15:00
 #SBATCH --mem=64G
 #SBATCH --account=project_2000859
